a67c695b5f2e9cdf49a53fb88d9130f6f47dbc2a
Brunoalura29 <bruno_hdias@hotmail.com>
2025-05-23 21:09:55 -0300

    docs: atualiza Readme e configurações do Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,10 +9,19 @@ const config: PlaywrightTestConfig = {
         screenshot: 'on',
     },
     projects: [
+        
         {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        }
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
     ],
 };
 export default config;
